a15cdad0fedbdcf4c137e9b13ddd00fac8dd72e2
Ekrem <ekremergun@outlook.com>
2021-12-28 22:29:51 +0300

    first commit

---
+++ TeaPreparationAlgorith/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using TeaPreparationAlgorith;
+
+TeaPreparationAsync teaPreparation = new();
+
+do
+{
+    var stopwatch = new Stopwatch();
+    stopwatch.Start();
+
+    var cayGroup = await teaPreparation.CayiPosettenCikar().ContinueWith(async _ =>
+    {
+        await teaPreparation.CayiUstDemligeKoy();
+        await teaPreparation.AltDemligeSuDoldur();
+
+    });
+
+    var ocakGroup = await teaPreparation.OcagiYak().ContinueWith(async _=>
+    {
+        await teaPreparation.DemliktekiSuyuKaynat();
+        await teaPreparation.IsinanSuyuUstDemligeKoyupCayiDemle();
+    });
+
+    await Task.WhenAll(cayGroup, ocakGroup);
+
+    await teaPreparation.AltDemligeTekrarSuKoyVeKisikAtesteBekle();
+
+    Console.WriteLine(String.Format("Bitiş Süresi : {0:N}", stopwatch.Elapsed.TotalSeconds));
+
+    stopwatch.Stop();
+
+    Console.WriteLine("Tekrar Demlemek İstermisin (E/H)");
+} while (Console.ReadKey().Key == ConsoleKey.E);
+
---
+++ TeaPreparationAlgorith/TeaPreparationAsync.cs
@@ -0,0 +1,41 @@
+﻿namespace TeaPreparationAlgorith
+{
+    public class TeaPreparationAsync
+    {
+        public async Task CayiPosettenCikar()
+        {
+            await Task.Delay(1000);
+            Console.WriteLine("Çayı poşetten çıkar.");
+        }
+        public async Task CayiUstDemligeKoy()
+        {
+            await Task.Delay(500);
+            Console.WriteLine("Çayı üst demliğe koy.");
+        }
+        public async Task AltDemligeSuDoldur()
+        {
+            await Task.Delay(1500);
+            Console.WriteLine("Alt demliğe su doldur.");
+        }
+        public async Task OcagiYak()
+        {
+            await Task.Delay(500);
+            Console.WriteLine("Ocağı yak.");
+        }
+        public async Task DemliktekiSuyuKaynat()
+        {
+            await Task.Delay(2000);
+            Console.WriteLine("Demlikteki suyu kaynat.");
+        }
+        public async Task IsinanSuyuUstDemligeKoyupCayiDemle()
+        {
+            await Task.Delay(1500);
+            Console.WriteLine("Isınan suyu üst demliğe koyup çayı demle.");
+        }
+        public async Task AltDemligeTekrarSuKoyVeKisikAtesteBekle()
+        {
+            await Task.Delay(2000);
+            Console.WriteLine("Alt demliğe tekrar su koy ve kısık ateşte bekle.");
+        }
+    }
+}
